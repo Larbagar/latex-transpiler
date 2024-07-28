5f136900a11d71c7553070df777759b5a17ff898
Larbagar <darcyvilkner@gmail.com>
2024-07-28 17:52:43 -0400

    Wrote some enums?

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,49 @@
 use std::fs;
 use std::env;
 
+
+enum BinaryOperator {
+    Addition,
+    Subtraction,
+    Multiplication,
+    Division,
+    // Mod?
+}
+
+enum Identifier {
+    Fraction,
+    Summation,
+}
+
+enum Symbol {
+    Superscript,
+    Subscript,
+}
+
+enum Group {
+    Group,
+    Parenthesis,
+    Bracket,
+    Brace,
+    Angle,
+}
+
+enum Token {
+    BinaryOperator(BinaryOperator),
+    Variable(String),
+    Begin(Group),
+    End(Group),
+}
+
 fn main() {
     let args = env::args().collect::<Vec<_>>();
 
     let path = &args[1];
 
     let contents = fs::read_to_string(path).expect("Should have been able to read file");
+
+    let token = Token{};
+
 
     println!("{contents}")
 }
